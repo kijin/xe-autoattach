1cf1962f238dfe6a158ca655b7dad6ed45746ebe
Kijin Sung <kijin@kijinsung.com>
2017-05-30 12:59:20 +0900

    Fix #15 document cache invalidation

--- autoattach.class.php
+++ autoattach.class.php
@@ -95,15 +95,17 @@ class XEAutoAttachAddon
 			$oDB->rollback();
 			return false;
 		}
+		
+		// Update cached entries.
 		if (!$get_fresh_object)
 		{
 			$oCachedDocument->add('content', $content);
 			$oCachedDocument->add('uploaded_count', $oDocument->uploaded_count + $count);
-			$oCacheHandler = CacheHandler::getInstance('object');
-			if($oCacheHandler->isSupport())
-			{
-				$oCacheHandler->delete('document_item:' . getNumberingPath($document_srl) . $document_srl);
-			}
+		}
+		$oCacheHandler = CacheHandler::getInstance('object');
+		if($oCacheHandler->isSupport())
+		{
+			$oCacheHandler->delete('document_item:' . getNumberingPath($document_srl) . $document_srl);
 		}
 		
 		// Commit!
